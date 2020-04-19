b495c10308ae7f9d7e82c461409f221720b8c0b8
hangorazvan <58172000+hangorazvan@users.noreply.github.com>
2020-04-19 10:08:23 +0300

    Update alarm.js

--- alarm.js
+++ alarm.js
@@ -19,28 +19,34 @@ Module.register("alarm",{
 		var self = this;
 	    setInterval(function() {
 
-	        var timer = moment().format('HH:mm:ss');
+	        var now = moment().format('HH:mm:ss');
 	        var hourly = moment().format('mm:ss');
-//	        var date = moment().format('DD-MM mm:ss');
+	        var date = moment().format('DD-MM mm:ss');
 
-    	    if (timer == '23:00:00') {
-    	        self.sendNotification("SHOW_ALERT", {type: "notification", title: "Noapte bună!", message: "Este timpul de culcare"});
+            if (now >= '00:00:00' && now < '06:59:59') {
+                $('body').css({'opacity':'0.4', '-webkit-filter':'grayscale(60%)'});
+            }
+            if ((now >= '07:00:00' && now < '07:59:59') || (now >= '23:00:00' && now < '23:59:59')) {
+                $('body').css({'opacity':'0.8', '-webkit-filter':'grayscale(30%)'});
+            }
+            if (now >= '08:00:00' && now < '22:59:59') {
+                $('body').css({'opacity':'1', '-webkit-filter':'grayscale(0%)'});
+            }
+    	    if ((now == '23:00:05') || (now == '00:00:05')) {
+    	        self.sendNotification("SHOW_ALERT", {type: "notification", title: "Noapte bună!", message: "Să ai un somn ușor"});
 		    }
-    	    if (timer == '00:00:00') {
-                self.sendNotification("SHOW_ALERT", {type: "notification", title: "Noapte bună!", message: "Să ai un somn ușor"});
-//     		    self.sendNotification("SHOW_ALERT", {timer: 5000, imageFA: "bed", title: "Noapte bună", message: "Să ai un somn ușor"});
+    	    if ((now == '07:00:05') || (now == '08:00:05')) {
+     		    self.sendNotification("SHOW_ALERT", {type: "notification", title: "Bună dimineața!", message: "Să ai o zi excelentă"});
 		    }
-    	    if (timer == '07:00:00') {
-     		    self.sendNotification("SHOW_ALERT", {type: "notification", title: "Bună dimineața!", message: "Este timpul de trezire"});
-//     		    self.sendNotification("SHOW_ALERT", {timer: 5000, imageFA: "bell", title: "Bună dimineața!", message: "Este timpul de trezire"});
+    	    if (hourly == '00:59') {
+    	        self.sendNotification("SHOW_ALERT", {type: "notification", title: "Ora exactă", message: "A fost ora " + moment().format('H:mm')});
 		    }
-    	    if (timer == '08:00:00') {
-    	        self.sendNotification("SHOW_ALERT", {type: "notification", title: "Bună dimineața!", message: "Să ai o zi excelentă"});
+    	    if ((date == '25-12 00:54') || (date == '26-12 00:54')) {
+    	        self.sendNotification("SHOW_ALERT", {type: "notification", title: "Crăciun fericit!", message: "Sărbători fericite cu multe bucurii!"});
 		    }
-    	    if (hourly == '00:00') {
-    	        self.sendNotification("SHOW_ALERT", {type: "notification", title: "Ora exactă", message: "A fost ora " + moment().format('H')});
+    	    if ((date == '01-01 00:54') || (date == '02-01 00:54')) {
+    	        self.sendNotification("SHOW_ALERT", {type: "notification", title: "La mulți ani " + moment().format('YYYY') + "!", message: "Un an nou prosper și multă sănătate!"});
 		    }
-
-	    }, 1000);
+	    }, 500);
 	},
 });
