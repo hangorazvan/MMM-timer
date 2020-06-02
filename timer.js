cbabb9c0902b2ac909444640683b36f1af1ade9d
Razvan Cristea <58172000+hangorazvan@users.noreply.github.com>
2020-06-02 10:53:16 +0300

    Update timer.js

--- timer.js
+++ timer.js
@@ -11,7 +11,7 @@ Module.register("timer", {
 		FadeMode: true, // fade to dimmed mode over night and back in the morning
 		NightMode: false, // zoomed night mode for iPad
 		FirstPoint: "23", // time of fade start increase
-		SecondPoint: "00", // time of fade stop decrease and night mode start
+		SecondPoint: "00", // time of fade stop increase and night mode start
 		ThirdPoint: "06", // time of fade start decrease
 		ForthPoint: "07", // time of fade mode stop decrease and day mode start
 		FifthPoint: "22", // time of day mode stop
