007bcb48fcb83b8389d341d402595b28bb03a4dd
Razvan Cristea <58172000+hangorazvan@users.noreply.github.com>
2020-06-17 20:44:01 +0300

    Update timer.js

--- timer.js
+++ timer.js
@@ -6,15 +6,15 @@
 
 Module.register("timer", {
 	defaults: {
-		SharpMode: true, // hourly alert notification
-		DateMode: true, // specific date hourly alert notification
-		FadeMode: true, // fade to dimmed mode over night and back in the morning
-		NightMode: false, // zoomed night mode for iPad
-		FirstPoint: "23", // time of fade start increase
-		SecondPoint: "00", // time of fade stop increase and night mode start
-		ThirdPoint: "06", // time of fade start decrease
-		ForthPoint: "07", // time of fade mode stop decrease and day mode start
-		FifthPoint: "22", // time of day mode stop
+		SharpMode: true,	// hourly alert notification
+		DateMode: true,		// specific date hourly alert notification
+		FadeMode: true,		// fade to dimmed mode over night and back in the morning
+		NightMode: false,	// zoomed night mode for iPad
+		FirstPoint: "23",	// time of fade start increase
+		SecondPoint: "00",	// time of fade stop increase and night mode start
+		ThirdPoint: "06",	// time of fade start decrease
+		ForthPoint: "07",	// time of fade mode stop decrease and day mode start
+		FifthPoint: "22",	// time of day mode stop
 	},
 	
 	getStyles: function() {
