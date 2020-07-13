d815e8c16563cf1b8dba80aec0e961d23ed221e6
Razvan Cristea <58172000+hangorazvan@users.noreply.github.com>
2020-07-13 21:44:00 +0300

    Add files via upload

--- timer.js
+++ timer.js
@@ -10,13 +10,14 @@ Module.register("timer", {
 		DateMode: true, // specific date hourly alert notification
 		FadeMode: true, // fade to dimmed mode over night and back in the morning
 		NightMode: false, // zoomed night mode for iPad
+		Background: false, // Background
 		FirstPoint: "23", // time of fade start increase
 		SecondPoint: "00", // time of fade stop increase and night mode start
 		ThirdPoint: "06", // time of fade start decrease
 		ForthPoint: "07", // time of fade mode stop decrease and day mode start
-		FifthPoint: "22", // time of day mode stop},
+		FifthPoint: "22", // time of day mode stop
 	},
-
+	
 	getScripts: function() {
 		return ["moment.js"];
 	},
