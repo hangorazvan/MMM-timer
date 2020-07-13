92bd555f2e17b3811bcdb5c6d5c506c295b4a7c6
Razvan Cristea <58172000+hangorazvan@users.noreply.github.com>
2020-07-13 21:45:19 +0300

    Add files via upload

--- timer.js
+++ timer.js
@@ -6,16 +6,16 @@
 
 Module.register("timer", {
 	defaults: {
-		SharpMode: true, // hourly alert notification
-		DateMode: true, // specific date hourly alert notification
-		FadeMode: true, // fade to dimmed mode over night and back in the morning
-		NightMode: false, // zoomed night mode for iPad
-		Background: false, // Background
-		FirstPoint: "23", // time of fade start increase
-		SecondPoint: "00", // time of fade stop increase and night mode start
-		ThirdPoint: "06", // time of fade start decrease
-		ForthPoint: "07", // time of fade mode stop decrease and day mode start
-		FifthPoint: "22", // time of day mode stop
+		SharpMode: true,	// hourly alert notification
+		DateMode: true,		// specific date hourly alert notification
+		FadeMode: true,		// fade to dimmed mode over night and back in the morning
+		NightMode: true,	// zoomed night mode for iPad
+		Background: true,	// Background image set for .fullscreen.below in css file
+		FirstPoint: "23",	// time of fade start increase
+		SecondPoint: "00",	// time of fade stop increase and night mode start
+		ThirdPoint: "06",	// time of fade start decrease
+		ForthPoint: "07",	// time of fade mode stop decrease and day mode start
+		FifthPoint: "22",	// time of day mode stop
 	},
 	
 	getScripts: function() {
