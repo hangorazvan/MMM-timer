397b06470b2fceaed805f627c44ad1ccfcef6ce6
Razvan Cristea <58172000+hangorazvan@users.noreply.github.com>
2020-08-24 21:07:51 +0300

    Add files via upload

--- timer.js
+++ timer.js
@@ -5,28 +5,7 @@
 */
 
 Module.register("timer", {
-	defaults: {
-		Bodysize: 1080,			// Minimum window width
-		SharpMode: true,		// hourly alert notification
-		DateMode: true,			// specific date hourly alert notification
-		FadeMode: true,			// fade to dimmed mode over night and back in the morning
-		DimMode: true,			// dimmed mode over night
-		NightMode: true,		// zoomed night mode for iPad 3
-		Background: false,		// background image set for .fullscreen.below in css file
-		Reload: false,			// reload page
-		Reinit: false,			// reinitialised MM
-		FirstPoint: "23",		// time of fade start increase
-		SecondPoint: "00",		// time of fade stop increase and night mode start
-		ThirdPoint: "06",		// time of fade start decrease and day mode start
-		ForthPoint: "07",		// time of fade mode stop decrease
-		FifthPoint: "22",		// time of day mode stop
-		Name1: "",		// Wife or girlfriend name
-		Birthday1: "",		// day & month
-		Name2: "",		// Husband or boyfriend name
-		Birthday2: "",		// day & month
-		Name3: "",			// child or pet name
-		Birthday3: "",			// day & month
-	},
+	defaults: {},
 	
 	getTranslations: function() {
 		return {
@@ -49,6 +28,7 @@ Module.register("timer", {
 			var dqs5 = Array.from(document.querySelectorAll(".pre-line"));
 			var dqs6 = Array.from(document.querySelectorAll(".monthly"));
 			var dqs7 = Array.from(document.querySelectorAll(".fullscreen.below"));
+			var dqs8 = Array.from(document.querySelectorAll(".ns-box"));
 			var body = Array.from(document.querySelectorAll("body"));
 			body.forEach(function(element) {return element.style["min-height"] =
 			window.innerHeight / (window.innerWidth / self.config.Bodysize) + "px",
@@ -67,65 +47,93 @@ Module.register("timer", {
 			}
 
 			if (self.config.SharpMode) {
-				if ((now == "23:00:00") || (now == "00:00:00") || (now == "01:00:00")) { self.sendNotification("SHOW_ALERT", {
-						type: "notification", title: "<i class=\"far fa-bell lime\"></i> " + self.translate("Sharp hour!"),
+				if ((now == "23:00:00") || (now == "00:00:00") || (now == "01:00:00")) {
+					self.sendNotification("SHOW_ALERT", {
+						type: "notification", 
+						title: "<i class=\"far fa-bell lime\"></i> " + self.translate("Sharp hour!"),
 						message: self.translate("Time it was ") + moment().format("H:mm") + "<br>" + self.translate("Good night!")
 					});
-				} else if ((now == "02:00:00") || (now == "03:00:00") || (now == "04:00:00")) { self.sendNotification("SHOW_ALERT", {
-						type: "notification", title: "<i class=\"far fa-bell lime\"></i> " + self.translate("Sharp hour!"),
+				} else if ((now == "02:00:00") || (now == "03:00:00") || (now == "04:00:00")) {
+					self.sendNotification("SHOW_ALERT", {
+						type: "notification", 
+						title: "<i class=\"far fa-bell lime\"></i> " + self.translate("Sharp hour!"),
 						message: self.translate("Time it was ") + moment().format("H:mm") + "<br>" + self.translate("Sleep well!")
 					});
 				} else if ((now == "05:00:00") || (now == "06:00:00") || (now == "07:00:00") || (now == "08:00:00") || 
-					(now == "09:00:00") || (now == "10:00:00") || (now == "11:00:00")) { self.sendNotification("SHOW_ALERT", {
-						type: "notification", title: "<i class=\"far fa-bell lime\"></i> " + self.translate("Sharp hour!"),
+					(now == "09:00:00") || (now == "10:00:00") || (now == "11:00:00")) {
+					self.sendNotification("SHOW_ALERT", {
+						type: "notification", 
+						title: "<i class=\"far fa-bell lime\"></i> " + self.translate("Sharp hour!"),
 						message: self.translate("Time it was ") + moment().format("H:mm") + "<br>" + self.translate("Good morning!")
 					});
-				} else if ((now == "12:00:00") || (now == "13:00:00") || (now == "14:00:00")) { self.sendNotification("SHOW_ALERT", {
-						type: "notification", title: "<i class=\"far fa-bell lime\"></i> " + self.translate("Sharp hour!"),
+				} else if ((now == "12:00:00") || (now == "13:00:00") || (now == "14:00:00")) {
+					self.sendNotification("SHOW_ALERT", {
+						type: "notification", 
+						title: "<i class=\"far fa-bell lime\"></i> " + self.translate("Sharp hour!"),
 						message: self.translate("Time it was ") + moment().format("H:mm") + "<br>" + self.translate("Bon appetit!")
 					});
-				} else if ((now == "15:00:00") || (now == "16:00:00") || (now == "17:00:00")) { self.sendNotification("SHOW_ALERT", {
-						type: "notification", title: "<i class=\"far fa-bell lime\"></i> " + self.translate("Sharp hour!"),
+				} else if ((now == "15:00:00") || (now == "16:00:00") || (now == "17:00:00")) {
+					self.sendNotification("SHOW_ALERT", {
+						type: "notification", 
+						title: "<i class=\"far fa-bell lime\"></i> " + self.translate("Sharp hour!"),
 						message: self.translate("Time it was ") + moment().format("H:mm") + "<br>" + self.translate("Have a nice day!")
 					});
-				} else if ((now == "18:00:00") || (now == "19:00:00") || (now == "20:00:00") || 
-					(now == "21:00:00") || (now == "22:00:00")) { self.sendNotification("SHOW_ALERT", {
-						type: "notification", title: "<i class=\"far fa-bell lime\"></i> " + self.translate("Sharp hour!"),
+				} else if ((now == "18:00:00") || (now == "19:00:00") || (now == "20:00:00") || (now == "21:00:00") || (now == "22:00:00")) {
+					self.sendNotification("SHOW_ALERT", {
+						type: "notification", 
+						title: "<i class=\"far fa-bell lime\"></i> " + self.translate("Sharp hour!"),
 						message: self.translate("Time it was ") + moment().format("H:mm") + "<br>" + self.translate("Have a nice evening!")
 					});
 				}
 			}
 
 			if (self.config.DateMode) { 
-				if ((date == "25-12 00:06") || (date == "26-12 00:06")) { self.sendNotification("SHOW_ALERT", {
-						type: "notification", timer: 9000, title: "<i class=\"fa fa-gifts yellow\"></i> " + self.translate("Marry Christmas!"),
+				if ((date == "25-12 00:06") || (date == "26-12 00:06")) {
+					self.sendNotification("SHOW_ALERT", {
+						type: "notification", timer: 8000, 
+						title: "<i class=\"fa fa-gifts yellow\"></i> " + self.translate("Marry Christmas!"),
 						message: self.translate("Happy holidays with many joys!")
 					});
-				} else if ((date == "01-01 00:06") || (date == "02-01 00:06")) { self.sendNotification("SHOW_ALERT", {
-						type: "notification", timer: 9000, title: "<i class=\"fa fa-glass-cheers yellow\"></i> " + self.translate("Happy Birthday ") + 
-						moment().format("YYYY") + "!", message: self.translate("A new year as good as possible and good health!")
+				} else if ((date == "01-01 00:06") || (date == "02-01 00:06")) {
+					self.sendNotification("SHOW_ALERT", {
+						type: "notification", timer: 8000, 
+						title: "<i class=\"fa fa-glass-cheers yellow\"></i> " + self.translate("Happy Birthday ") + moment().format("YYYY") + "!", 
+						message: self.translate("A new year as good as possible and good health!")
 					});
-				} else if (date == "14-02 00:06") { self.sendNotification("SHOW_ALERT", {
-						type: "notification", timer: 9000, title: "<i class=\"far fa-heart redrf\"></i> " + self.translate("Happy Valentine's Day!"),
+				} else if (date == "14-02 00:06") {
+					self.sendNotification("SHOW_ALERT", {
+						type: "notification", timer: 8000, 
+						title: "<i class=\"far fa-heart redrf\"></i> " + self.translate("Happy Valentine's Day!"),
 						message: self.translate("Happy Valentine's and much happiness!")
 					});
-				} else if (date == self.config.Birthday1 + " 00:06") { self.sendNotification("SHOW_ALERT", {
-						type: "notification", timer: 9000, title: "<i class=\"fa fa-birthday-cake yellow\"></i> " + self.translate("Happy Birthday, ") + 
-						self.config.Name1, message: self.translate("Good health and be happy! F")
+				} else if (date == self.config.Birthday1 + " 00:06") {
+					self.sendNotification("SHOW_ALERT", {
+						type: "notification", timer: 8000, 
+						title: "<i class=\"fa fa-birthday-cake yellow\"></i> " + self.translate("Happy Birthday, ") + self.config.Name1, 
+						message: self.translate("Good health and be happy! F")
 					});
-				} else if (date == self.config.Birthday2 + " 00:06") { self.sendNotification("SHOW_ALERT", {
-						type: "notification", timer: 9000, title: "<i class=\"fa fa-birthday-cake yellow\"></i> " + self.translate("Happy Birthday, ") + 
-						self.config.Name2, message: self.translate("Good health and be happy! M")
+				} else if (date == self.config.Birthday2 + " 00:06") {
+					self.sendNotification("SHOW_ALERT", {
+						type: "notification", timer: 8000, 
+						title: "<i class=\"fa fa-birthday-cake yellow\"></i> " + self.translate("Happy Birthday, ") + self.config.Name2, 
+						message: self.translate("Good health and be happy! M")
 					});
-				} else if (date == self.config.Birthday3 + " 00:06") { self.sendNotification("SHOW_ALERT", {
-						type: "notification", timer: 9000, title: "<i class=\"fa fa-birthday-cake yellow\"></i> " + self.translate("Happy Birthday, ") + 
-						self.config.Name3, message: self.translate("Good health and be happy! M")
+				} else if (date == self.config.Birthday3 + " 00:06") {
+					self.sendNotification("SHOW_ALERT", {
+						type: "notification", timer: 8000, 
+						title: "<i class=\"fa fa-birthday-cake yellow\"></i> " + self.translate("Happy Birthday, ") + self.config.Name3, 
+						message: self.translate("Good health and be happy! M")
 					});
 				}
 			}
-			
+/*			
 			if ((hide == "16") || (hide == "31") || (hide == "46")) {
 				self.sendNotification("HIDE_ALERT");
+				dqs8.forEach(function(element) {return element.classList.remove("ns-show"), element.classList.add("ns-hide");});
+			}
+*/
+			if (hide == "59") {
+				dqs8.forEach(function(element) {element.style.display = "none";});
 			}
 
 			if (self.config.NightMode) {
