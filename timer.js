d3ea4159a6c327566429d96b8dd5c5883d2229a7
Razvan Cristea <58172000+hangorazvan@users.noreply.github.com>
2020-09-07 11:12:44 +0300

    Update timer.js

--- timer.js
+++ timer.js
@@ -30,10 +30,7 @@ Module.register("timer", {
 	},
 	
 	getTranslations: function() {
-		return {
-			en: "en.json",
-			ro: "ro.json",
-		};
+		return {en: "en.json", ro: "ro.json",};
 	},
 
 	start: function() {
@@ -56,53 +53,39 @@ Module.register("timer", {
 			window.innerHeight / (window.innerWidth / self.config.Bodysize) + "px",
 			element.style["min-width"] = self.config.Bodysize + "px";});
 
-			if (self.config.Reload) {
-				if (now == self.config.SecondPoint) {
-					location.reload();
-				}
-			}
+			if (self.config.Reload) { if (now == self.config.SecondPoint) {location.reload();}}
 
-			if (self.config.Reinit) {
-				if (now == self.config.SecondPoint) {
-					MM.init();
-				}
-			}
+			if (self.config.Reinit) { if (now == self.config.SecondPoint) {MM.init();}}
 
 			if (self.config.SharpMode) {
 				if ((now == "23:00:00") || (now == "00:00:00") || (now == "01:00:00")) {
-					self.sendNotification("SHOW_ALERT", {
-						type: "notification", 
+					self.sendNotification("SHOW_ALERT", {type: "notification", 
 						title: "<i class=\"far fa-bell lime\"></i> " + self.translate("Sharp hour!"),
 						message: self.translate("Time it was ") + moment().format("H:mm") + "<br>" + self.translate("Good night!")
 					});
 				} else if ((now == "02:00:00") || (now == "03:00:00") || (now == "04:00:00")) {
-					self.sendNotification("SHOW_ALERT", {
-						type: "notification", 
+					self.sendNotification("SHOW_ALERT", {type: "notification", 
 						title: "<i class=\"far fa-bell lime\"></i> " + self.translate("Sharp hour!"),
 						message: self.translate("Time it was ") + moment().format("H:mm") + "<br>" + self.translate("Sleep well!")
 					});
 				} else if ((now == "05:00:00") || (now == "06:00:00") || (now == "07:00:00") || (now == "08:00:00") || 
 					(now == "09:00:00") || (now == "10:00:00") || (now == "11:00:00")) {
-					self.sendNotification("SHOW_ALERT", {
-						type: "notification", 
+					self.sendNotification("SHOW_ALERT", {type: "notification", 
 						title: "<i class=\"far fa-bell lime\"></i> " + self.translate("Sharp hour!"),
 						message: self.translate("Time it was ") + moment().format("H:mm") + "<br>" + self.translate("Good morning!")
 					});
 				} else if ((now == "12:00:00") || (now == "13:00:00") || (now == "14:00:00")) {
-					self.sendNotification("SHOW_ALERT", {
-						type: "notification", 
+					self.sendNotification("SHOW_ALERT", {type: "notification", 
 						title: "<i class=\"far fa-bell lime\"></i> " + self.translate("Sharp hour!"),
 						message: self.translate("Time it was ") + moment().format("H:mm") + "<br>" + self.translate("Bon appetit!")
 					});
 				} else if ((now == "15:00:00") || (now == "16:00:00") || (now == "17:00:00")) {
-					self.sendNotification("SHOW_ALERT", {
-						type: "notification", 
+					self.sendNotification("SHOW_ALERT", {type: "notification", 
 						title: "<i class=\"far fa-bell lime\"></i> " + self.translate("Sharp hour!"),
 						message: self.translate("Time it was ") + moment().format("H:mm") + "<br>" + self.translate("Have a nice day!")
 					});
 				} else if ((now == "18:00:00") || (now == "19:00:00") || (now == "20:00:00") || (now == "21:00:00") || (now == "22:00:00")) {
-					self.sendNotification("SHOW_ALERT", {
-						type: "notification", 
+					self.sendNotification("SHOW_ALERT", {type: "notification", 
 						title: "<i class=\"far fa-bell lime\"></i> " + self.translate("Sharp hour!"),
 						message: self.translate("Time it was ") + moment().format("H:mm") + "<br>" + self.translate("Have a nice evening!")
 					});
@@ -110,56 +93,41 @@ Module.register("timer", {
 			}
 
 			if (self.config.DateMode) { 
-				if ((date == "25-12 00:06") || (date == "26-12 00:06")) {
-					self.sendNotification("SHOW_ALERT", {
-						type: "notification", timer: 8000, 
+				if ((date == "25-12 00:06") || (date == "26-12 00:06")) {self.sendNotification("SHOW_ALERT", {type: "notification", timer: 8000, 
 						title: "<i class=\"fa fa-gifts yellow\"></i> " + self.translate("Marry Christmas!"),
 						message: self.translate("Happy holidays with many joys!")
 					});
-				} else if ((date == "01-01 00:06") || (date == "02-01 00:06")) {
-					self.sendNotification("SHOW_ALERT", {
-						type: "notification", timer: 8000, 
+				} else if ((date == "01-01 00:06") || (date == "02-01 00:06")) {self.sendNotification("SHOW_ALERT", {type: "notification", timer: 8000, 
 						title: "<i class=\"fa fa-glass-cheers yellow\"></i> " + self.translate("Happy Birthday ") + moment().format("YYYY") + "!", 
 						message: self.translate("A new year as good as possible and good health!")
 					});
-				} else if (date == "14-02 00:06") {
-					self.sendNotification("SHOW_ALERT", {
-						type: "notification", timer: 8000, 
+				} else if (date == "14-02 00:06") {self.sendNotification("SHOW_ALERT", {type: "notification", timer: 8000, 
 						title: "<i class=\"far fa-heart redrf\"></i> " + self.translate("Happy Valentine's Day!"),
 						message: self.translate("Happy Valentine's and much happiness!")
 					});
-				} else if (date == self.config.Birthday1 + " 00:06") {
-					self.sendNotification("SHOW_ALERT", {
-						type: "notification", timer: 8000, 
+				} else if (date == self.config.Birthday1 + " 00:06") {self.sendNotification("SHOW_ALERT", {type: "notification", timer: 8000, 
 						title: "<i class=\"fa fa-birthday-cake yellow\"></i> " + self.translate("Happy Birthday, ") + self.config.Name1, 
 						message: self.translate("Good health and be happy! F")
 					});
-				} else if (date == self.config.Birthday2 + " 00:06") {
-					self.sendNotification("SHOW_ALERT", {
-						type: "notification", timer: 8000, 
+				} else if (date == self.config.Birthday2 + " 00:06") {self.sendNotification("SHOW_ALERT", {type: "notification", timer: 8000, 
 						title: "<i class=\"fa fa-birthday-cake yellow\"></i> " + self.translate("Happy Birthday, ") + self.config.Name2, 
 						message: self.translate("Good health and be happy! M")
 					});
-				} else if (date == self.config.Birthday3 + " 00:06") {
-					self.sendNotification("SHOW_ALERT", {
-						type: "notification", timer: 8000, 
+				} else if (date == self.config.Birthday3 + " 00:06") {self.sendNotification("SHOW_ALERT", {type: "notification", timer: 8000, 
 						title: "<i class=\"fa fa-birthday-cake yellow\"></i> " + self.translate("Happy Birthday, ") + self.config.Name3, 
 						message: self.translate("Good health and be happy! M")
 					});
 				}
 			}
-/*			
-			if ((hide == "16") || (hide == "31") || (hide == "46")) {
-				self.sendNotification("HIDE_ALERT");
+
+/*			not working !?
+			if ((hide == "16") || (hide == "31") || (hide == "46")) {self.sendNotification("HIDE_ALERT");
 				dqs8.forEach(function(element) {return element.classList.remove("ns-show"), element.classList.add("ns-hide");});
 			}
 */
-			if (hide == "59") {
-				dqs8.forEach(function(element) {element.style.display = "none";});
-			}
+			if (hide == "59") {dqs8.forEach(function(element) {element.style.display = "none";});}
 
-			if (self.config.NightMode) {
-				if (window.innerWidth < self.config.Bodysize) {
+			if (self.config.NightMode) { if (window.innerWidth < self.config.Bodysize) {
 					if (now >= self.config.SecondPoint + ":00:00" && now < self.config.ThirdPoint + ":00:00") {
 						body.forEach(function(element) {element.style.transform = "scale(" + window.innerWidth / self.config.Bodysize * 1.55 + ")";});
 						dqs1.forEach(function(element) {element.style.display = "none";});
@@ -167,8 +135,8 @@ Module.register("timer", {
 						dqs3.forEach(function(element) {element.style["margin-left"] = "-15px";});
 						dqs4.forEach(function(element) {return element.style.position = "relative", element.style.left = "-720px",
 						element.style.top = "270px", element.style["text-align"] = "left";});
-						dqs5.forEach(function(element) {return element.style.position = "relative",
-						element.style.left = "390px", element.style.top = "-160px", element.style["font-size"] = "3.5rem",
+						dqs5.forEach(function(element) {return element.style.position = "absolute",
+						element.style.left = "390px", element.style.top = "-350px", element.style["font-size"] = "3.5rem",
 						element.style["line-height"] = "4rem", element.style.width = "300px";});
 						dqs6.forEach(function(element) {element.style.display = "none";});
 					} else if (now >= self.config.ThirdPoint + ":00:01" && now < self.config.FirstPoint + ":59:59") {
@@ -183,44 +151,36 @@ Module.register("timer", {
 						element.style["line-height"] = "6.8rem", element.style.width = "initial";});
 						dqs6.forEach(function(element) {element.style.display = "table";});
 					}
-				} else {
-					body.forEach(function(element) {element.style.transform = "scale(1)";});
+				} else {body.forEach(function(element) {element.style.transform = "scale(1)";});
+						dqs1.forEach(function(element) {element.style.display = "initial";});
+						dqs2.forEach(function(element) {element.style.float = "left";});
+						dqs3.forEach(function(element) {element.style["margin-left"] = "initial";});
+						dqs4.forEach(function(element) {return element.style.position = "initial", element.style.left = "initial",
+						element.style.top = "initial", element.style["text-align"] = "right";});
+						dqs5.forEach(function(element) {return element.style.position = "initial",
+						element.style.left = "initial", element.style.top = "initial", element.style["font-size"] = "6.4rem",
+						element.style["line-height"] = "6.8rem", element.style.width = "initial";});
+						dqs6.forEach(function(element) {element.style.display = "table";});
 				}
-			} else { 
-				if (window.innerWidth < self.config.Bodysize){
+			} else { if (window.innerWidth < self.config.Bodysize) {
 					body.forEach(function(element) {element.style.transform = "scale(" + window.innerWidth / self.config.Bodysize + ")";});
-				} else { 
-					body.forEach(function(element) {element.style.transform = "scale(1)";});
-				}
+				} else {body.forEach(function(element) {element.style.transform = "scale(1)";});}
 			}
 			
 			if (self.config.Burning) {
-			    if (hide == "00") {
-				    body.forEach(function(element) {return element.style["margin-right"] = "0px", element.style["margin-top"] = "1px";});
-			    }
-			    if (hide == "15") {
-				    body.forEach(function(element) {return element.style["margin-top"] = "0px", element.style["margin-left"] = "1px";});
-			    }
-			    if (hide == "30") {
-				    body.forEach(function(element) {return element.style["margin-left"] = "0px", element.style["margin-top"] = "-1px";});
-			    }
-			    if (hide == "45") {
-				    body.forEach(function(element) {return element.style["margin-top"] = "0px", element.style["margin-right"] = "1px";});
-			    }
+			    if (hide == "00") {body.forEach(function(element) {return element.style["margin-right"] = "0px", element.style["margin-top"] = "1px";});}
+			    if (hide == "15") {body.forEach(function(element) {return element.style["margin-top"] = "0px", element.style["margin-left"] = "1px";});}
+			    if (hide == "30") {body.forEach(function(element) {return element.style["margin-left"] = "0px", element.style["margin-top"] = "-1px";});}
+			    if (hide == "45") {body.forEach(function(element) {return element.style["margin-top"] = "0px", element.style["margin-right"] = "1px";});}
 			}
 
 			if (self.config.Background) {
 				if (now >= self.config.SecondPoint + ":00:00" && now < self.config.ThirdPoint + ":00:00") {
 					dqs7.forEach(function(element) {element.style.backgound = "transparent";});
-				} else {
-					dqs7.forEach(function(element) {element.style.background = "transparent url(\"css/earth.png\") no-repeat center";});
-				}
-			} else {
-				dqs7.forEach(function(element) {element.style.background = "transparent";});
-			}
+				} else {dqs7.forEach(function(element) {element.style.background = "transparent url(\"css/earth.png\") no-repeat center";});}
+			} else {dqs7.forEach(function(element) {element.style.background = "transparent";});}
 
-			if (self.config.DimMode) {
-				if (self.config.FadeMode) {
+			if (self.config.DimMode) { if (self.config.FadeMode) {
 					if (now >= self.config.FirstPoint + ":00:00" && now < self.config.FirstPoint + ":59:59") {
 						body.forEach(function(element) {return element.style.opacity = opac1, element.style.filter = "grayscale(" + gray1 + "%)";});
 					} else if (now >= self.config.SecondPoint + ":00:00" && now < self.config.ThirdPoint + ":00:00") {
@@ -230,8 +190,7 @@ Module.register("timer", {
 					} else if (now >= self.config.ForthPoint + ":00:00" && now < self.config.FifthPoint + ":59:59") {
 						body.forEach(function(element) {return element.style.opacity = "1", element.style.filter = "grayscale(0%)";});
 					}
-				} else { 
-					if (now >= self.config.SecondPoint + ":00:00" && now < self.config.ThirdPoint + ":59:59") {
+				} else { if (now >= self.config.SecondPoint + ":00:00" && now < self.config.ThirdPoint + ":59:59") {
 						body.forEach(function(element) {return element.style.opacity = "0.50", element.style.filter = "grayscale(50%)";});
 					} else if (now >= self.config.ForthPoint + ":00:00" && now < self.config.FifthPoint + ":59:59") {
 						body.forEach(function(element) {return element.style.opacity = "1", element.style.filter = "grayscale(0%)";});
