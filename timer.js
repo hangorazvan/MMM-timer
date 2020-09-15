86cf96560fa9b76fc4c06a988008dbe078a9e8a5
Razvan Cristea <58172000+hangorazvan@users.noreply.github.com>
2020-09-15 20:31:45 +0300

    Add files via upload

--- timer.js
+++ timer.js
@@ -5,11 +5,26 @@
 */
 
 Module.register("timer", {
-	defaults: {},
+	defaults: {
+		bodysize: 1080,		// Minimum window width
+		sharpMode: true,	// hourly alert notification
+		dateMode: true,		// specific date hourly custom notification
+		fadeMode: true,		// fade to dimmed mode over night and back in the morning
+		dimmMode: true,		// dimmed mode over night
+		nightMode: true,	// zoomed night mode for iPad 3
+		dimming: 40,		// dimming amount (default is 40% grayscale with 0.6 opacity but you can change to w.e.y.w. ex: 70% grayscale with 0.3 opacity)
+		midnight: 24,		// midnight or custom timer start
+		name1: "",			// Wife or girlfriend name
+		birthday1: "",		// day & month
+		name2: "",			// Husband or boyfriend name
+		birthday2: "",		// day & month
+		name3: "",			// Child or pet name
+		birthday3: ""		// day & month
+	},
 	
-//	getScripts: function() {
-//		return ["moment.js"];
-//	},
+	getScripts: function() {
+		return ["moment.js"];
+	},
 
 	getTranslations: function() {
 		return {en: "en.json", ro: "ro.json",};
